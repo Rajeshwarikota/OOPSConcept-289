6e7249e4841e405696967efe088339b116faad46
“Rajeshwari <kotarajeswari003@gmail.com>
2023-06-27 19:57:19 +0530

    [Rajeshwari kota] Adding Types of Methods

--- OOPSConcepts289/Program.cs
+++ OOPSConcepts289/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOPSConcepts289.Class_and_Object;
+using OOPSConcepts289.Typesofmethods;
 using OOPSConcepts289.TypesOfVariables;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace OOPSConcepts289
         {
             Console.WriteLine("....OOPS CONCEPT....");
             Console.WriteLine("choose the below Options");
-            Console.WriteLine("1:Class And Object \n2:Types Of Variables");
+            Console.WriteLine("1:Class And Object \n2:Types Of Variables\n3:Types of Methods");
             int num = Convert.ToInt32(Console.ReadLine());
 
             switch (num)
@@ -33,6 +34,17 @@ namespace OOPSConcepts289
                     Console.WriteLine("Constant Variables are : " + Variables.RAM);
                     Console.WriteLine("Readonly Variables are : " + var.Battery);
                     break;
+               case 3:
+                    Method m = new Method();
+                    m.Brand = "Bata";
+                    m.warranty = 4;
+                    Console.WriteLine("Brand Name is : " + m.Brand);
+                    Console.WriteLine("Warranty years : " + m.warranty);
+                    Method.Quality = "Excellent";
+                    Console.WriteLine("quality should be : " + Method.Quality);
+                    Method.Work(); // static method calling
+                    m.Usage(); // Instance method calling
+                    break; 
             }
             Console.ReadLine();
         }
---
+++ OOPSConcepts289/Typesofmethods/Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts289.Typesofmethods
+{
+    class Method
+    {
+        public string Brand; // instance Variables
+        public int warranty; // instance Variables
+        public static string Quality ; //static variable
+
+        // Instance Method
+        public void Usage()
+        {
+            Console.WriteLine("Usage should be Proper");
+        }
+
+        // Static method
+        public static void Work()
+        {
+            Console.WriteLine("Comfortable working");
+        }
+    }
+}
