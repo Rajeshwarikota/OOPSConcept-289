94e34c72c2f06696ebe1181b81cacee6d218c849
“Rajeshwari <kotarajeswari003@gmail.com>
2023-06-27 18:57:44 +0530

    [Rajeshwari kota] Adding Types of variables

--- OOPSConcepts289/Program.cs
+++ OOPSConcepts289/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOPSConcepts289.Class_and_Object;
+using OOPSConcepts289.TypesOfVariables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +13,29 @@ namespace OOPSConcepts289
         static void Main(string[] args)
         {
             Console.WriteLine("....OOPS CONCEPT....");
-            Organization org = new Organization();  //Object Creation
-            org.BatchNumber = 289;
-            org.Name = "Raji";
-            Console.WriteLine(org.Name + " is belongs to " + org.BatchNumber);
+            Console.WriteLine("choose the below Options");
+            Console.WriteLine("1:Class And Object \n2:Types Of Variables");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Organization org = new Organization();  //Object Creation
+                    org.BatchNumber = 289;
+                    org.Name = "Raji";
+                    Console.WriteLine(org.Name + " is belongs to " + org.BatchNumber);
+                    break;
+                case 2:
+                    Variables var = new Variables();
+                    var.Adding();
+                    Console.WriteLine("Instance Variables are : " + var.Cost);
+                    Console.WriteLine("Static variables are : " + Variables.Device);
+                    Console.WriteLine("Constant Variables are : " + Variables.RAM);
+                    Console.WriteLine("Readonly Variables are : " + var.Battery);
+                    break;
+            }
             Console.ReadLine();
         }
     }
 }
+
---
+++ OOPSConcepts289/TypesOfVariables/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts289.TypesOfVariables
+{
+   public  class Variables
+    {
+        public static string Device = "Mobile"; //static variable
+        public int Cost = 200000; // instance variable
+        public const int RAM = 64; // Constant variable
+        public readonly float Battery = 5000; //ReadOnly Variables 
+    
+        public void Adding()
+        {
+            // Local Variables
+            int num1 = 12;
+            int num2 = 13;
+            int result = num1 + num2;
+            Console.WriteLine("Local Variables : " + result);
+        }
+    }
+}
+
